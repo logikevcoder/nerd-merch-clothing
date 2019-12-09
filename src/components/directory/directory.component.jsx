d46ac3378cce36fbc9711889cc16a29913ea73d4
Kevin Napier <kevin.napier@rotary.org>
2019-12-09 12:07:14 -0600

    Configured final styles for menu items, added Open Sans font, removed unneeded files/images from the CRA setup

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import MenuItem from "../menu-item/menu-item.component";
-import "./directory.styles";
+import "./directory.styles.scss";
 
 class Directory extends Component {
   state = {
@@ -8,34 +8,34 @@ class Directory extends Component {
       {
         title: "hats",
         imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-        id: 1
-        // linkUrl: "shop/hats"
+        id: 1,
+        linkUrl: "shop/hats"
       },
       {
         title: "jackets",
         imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-        id: 2
-        // linkUrl: "shop/jackets"
+        id: 2,
+        linkUrl: "shop/jackets"
       },
       {
         title: "sneakers",
         imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-        id: 3
-        // linkUrl: "shop/sneakers"
+        id: 3,
+        linkUrl: "shop/sneakers"
       },
       {
         title: "womens",
         imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-        // size: "large",
-        id: 4
-        // linkUrl: "shop/womens"
+        size: "large",
+        id: 4,
+        linkUrl: "shop/womens"
       },
       {
         title: "mens",
         imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-        // size: "large",
-        id: 5
-        // linkUrl: "shop/mens"
+        size: "large",
+        id: 5,
+        linkUrl: "shop/mens"
       }
     ]
   };
@@ -43,14 +43,15 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id }) => {
+        {this.state.sections.map(({ title, imageUrl, id, size }) => (
           <MenuItem
             key={id}
             title={title}
             imageUrl={imageUrl}
+            size={size}
             cta={"SHOP NOW"}
-          />;
-        })}
+          />
+        ))}
       </div>
     );
   }
