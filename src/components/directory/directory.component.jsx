57d943eaf87493d7293555e0219298b8005efc30
“Kevin <logikevcoder@gmail.com>
2019-12-09 22:00:31 -0600

    updated menu-item component to link url and history through withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,41 +1,41 @@
-import React, { Component } from "react";
-import MenuItem from "../menu-item/menu-item.component";
-import "./directory.styles.scss";
+import React, { Component } from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+import './directory.styles.scss';
 
 class Directory extends Component {
   state = {
     sections: [
       {
-        title: "hats",
-        imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+        title: 'hats',
+        imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
         id: 1,
-        linkUrl: "shop/hats"
+        linkUrl: 'shop/hats'
       },
       {
-        title: "jackets",
-        imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+        title: 'jackets',
+        imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
         id: 2,
-        linkUrl: "shop/jackets"
+        linkUrl: 'shop/jackets'
       },
       {
-        title: "sneakers",
-        imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+        title: 'sneakers',
+        imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
         id: 3,
-        linkUrl: "shop/sneakers"
+        linkUrl: 'shop/sneakers'
       },
       {
-        title: "womens",
-        imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-        size: "large",
+        title: 'womens',
+        imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+        size: 'large',
         id: 4,
-        linkUrl: "shop/womens"
+        linkUrl: 'shop/womens'
       },
       {
-        title: "mens",
-        imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-        size: "large",
+        title: 'mens',
+        imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+        size: 'large',
         id: 5,
-        linkUrl: "shop/mens"
+        linkUrl: 'shop/mens'
       }
     ]
   };
@@ -43,14 +43,8 @@ class Directory extends Component {
   render() {
     return (
       <div className="directory-menu">
-        {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem
-            key={id}
-            title={title}
-            imageUrl={imageUrl}
-            size={size}
-            cta={"SHOP NOW"}
-          />
+        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+          <MenuItem key={id} {...otherSectionProps} />
         ))}
       </div>
     );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,13 @@
-import React from "react";
-import "./menu-item.styles.scss";
+import React from 'react';
+import { withRouter } from 'react-router-dom';
+import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size, cta }) => {
+const MenuItem = ({ title, imageUrl, size, cta, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
       <div
         className="background-image"
         style={{
@@ -18,4 +22,4 @@ const MenuItem = ({ title, imageUrl, size, cta }) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
