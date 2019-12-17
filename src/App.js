80ec3ea5e6dfa180232aee9031a61d3a2d40c146
“Kevin <logikevcoder@gmail.com>
2019-12-17 07:30:26 -0600

    setup firebase signing with google

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import "./App.css";
-import Header from "./components/header/header.component";
-import HomePage from "./pages/homepage/homepage.component";
-import ShopPage from "./components/shop/shop.component";
-import SignInandSignUpPage from "./components/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import './App.css';
+import Header from './components/header/header.component';
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './components/shop/shop.component';
+import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
-        <Route exact path="/signIn" component={SignInandSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    currentUser: null
+  };
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log('user :', user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signIn" component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,17 +1,18 @@
-import React, { Component } from "react";
-import "./sign-in.styles.scss";
-import FormInput from "../form-input/form-input.component";
-import CustomButton from "../custom-button/custom-button.component";
+import React, { Component } from 'react';
+import './sign-in.styles.scss';
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 class SignIn extends Component {
   state = {
-    email: "",
-    password: ""
+    email: '',
+    password: ''
   };
 
   handleSubmit = e => {
     e.preventDefault();
-    this.setState({ email: "", password: "" });
+    this.setState({ email: '', password: '' });
   };
 
   handleChange = e => {
@@ -43,6 +44,7 @@ class SignIn extends Component {
             required
           />
           <CustomButton type="submit">Sign In</CustomButton>
+          <CustomButton onClick={signInWithGoogle}>Sign In</CustomButton>
         </form>
       </div>
     );
