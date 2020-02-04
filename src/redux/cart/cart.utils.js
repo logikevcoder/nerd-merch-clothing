b0118e4e90486bc568764363c51ea7f455b0a671
Kevin Napier <Kevin.Napier@rotary.org>
2020-02-04 12:47:50 -0600

    created cartItem component, created cart.utils.js file, added CartItem to the CartDropdown

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,12 +1,14 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
-  const existingCartItem = cartItems.find(
-    cartItem => cartItem.id === cartItemToAdd.id
+  // set this const variable to be the cartItem where the id matches the item to add
+  const existingCartItem = cartItems.find( // if this is true return below
+    cartItem => cartItem.id === cartItemToAdd.id //get each individual item, if the id matches the item to add's id
   );
 
+  // if the item exists above 
   if (existingCartItem) {
-    return cartItems.map(cartItem =>
+    return cartItems.map(cartItem => 
       cartItem.id === cartItemToAdd.id
-        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        ? { ...cartItem, quantity: cartItem.quantity + 1 } // return new object with the quantity + 1
         : cartItem
     );
   }
