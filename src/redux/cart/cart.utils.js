7db09cab3eec037a84f2bd6e79a56b4a58a20456
Kevin Napier <kevin.napier@rotary.org>
2020-02-10 11:02:43 -0600

    added functionality to remove cart item

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -1,12 +1,13 @@
 export const addItemToCart = (cartItems, cartItemToAdd) => {
   // set this const variable to be the cartItem where the id matches the item to add
-  const existingCartItem = cartItems.find( // if this is true return below
+  const existingCartItem = cartItems.find(
+    // if this is true return below
     cartItem => cartItem.id === cartItemToAdd.id //get each individual item, if the id matches the item to add's id
   );
 
-  // if the item exists above 
+  // if the item exists above
   if (existingCartItem) {
-    return cartItems.map(cartItem => 
+    return cartItems.map(cartItem =>
       cartItem.id === cartItemToAdd.id
         ? { ...cartItem, quantity: cartItem.quantity + 1 } // return new object with the quantity + 1
         : cartItem
@@ -17,20 +18,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 };
 
 export const removeItemFromCart = (cartItems, cartItemToRemove) => {
-  // set this const variable to be the cartItem where the id matches the item to add
-  const existingCartItem = cartItems.find( // if this is true return below
-    cartItem => cartItem.id === cartItemToRemove.id //get each individual item, if the id matches the item to add's id
+  // set this const variable to be the cartItem where the id matches the item to remove
+  const existingCartItem = cartItems.find(
+    // if this is true return below
+    cartItem => cartItem.id === cartItemToRemove.id //get each individual item, if the id matches the item to remove's id
   );
 
-  // if the item exists above 
-  if (existingCartItem) {
-    return cartItems.map(cartItem => 
-      cartItem.id === cartItemToRemove.id
-        ? { ...cartItem, quantity: cartItem.quantity === 0 }
-        : cartItem
-    );
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id);
   }
 
-  return [...cartItems, { ...cartItemToRemove, quantity: 0 }];
+  return cartItems.map(cartItem =>
+    cartItem.id === cartItemToRemove.id
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
 };
-
